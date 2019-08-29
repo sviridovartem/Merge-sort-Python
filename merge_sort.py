d436be7e56687f29344a2e28d5b2b72170aa8e17
Svyrydov Artem <a.svyrydov@quantimobile.com>
2019-08-29 13:44:15 +0300

    ID 9315 doc-string test

--- merge_sort.py
+++ merge_sort.py
@@ -1,4 +1,5 @@
 import logging
+import doctest
 
 
 def merge(input_list, left_part, right_part):
@@ -33,9 +34,20 @@ def merge(input_list, left_part, right_part):
 
 
 def merge_sort(input_list):
+    """Возвращает факториал числа n, которое является числом >= 0.
+
+        Если резульатат умещается в int, возвращается int.
+        Иначе возвращается long.
+
+        >>> merge_sort([15, 13, 11, 16, 18, 69, 46, 22, 11, 10, 8, 5, 8])
+        [5, 8, 8, 10, 11, 11, 13, 15, 16, 18, 22, 46, 69]
+        >>> merge_sort([15, 13, 11, 16, 18, 69, 46, 22, 11, 10, 8, 5, 9])
+        [5, 8, 9, 10, 11, 11, 13, 15, 16, 18, 22, 46, 69]
+
+        """
+
     logging.info('Inside splitting part')
     if len(input_list) > 1:
-
         left_part = input_list[:len(input_list) // 2]
         right_part = input_list[len(input_list) // 2:]
 
@@ -45,7 +57,7 @@ def merge_sort(input_list):
         logging.info('Starting merging')
         merge(input_list, left_part, right_part)
         logging.info('Finished merging')
-
+    return input_list
 
 def main():
     logging.basicConfig(filename='merge_sort.log', filemode='w', level=logging.INFO)
@@ -55,9 +67,11 @@ def main():
     print(test_list)
 
     merge_sort(test_list)
-    print(test_list)
+    print("return {}".format(merge_sort(test_list)))
 
     logging.info('Finished')
 
+
 if __name__ == '__main__':
     main()
+    doctest.testmod()
